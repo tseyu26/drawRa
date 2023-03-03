9938a943e7a7feab0066df51f513d604a9645d16
Tseyu Yang <121663430+tseyu26@users.noreply.github.com>
2023-03-03 16:04:01 +0800

    Add project files.

---
+++ main.py
@@ -0,0 +1,6 @@
+print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+print("          This program is designed to draw the Ra picture\n")
+print("                    Created by tseyu on 3/3/2023\n")
+print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+
+import readCsv
---
+++ readCsv.py
@@ -0,0 +1,41 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+fileName = input("Input the name of the file: ")
+print("The name of the csv file is: " + fileName )
+
+print("Reading...")
+
+strLength = []
+strHeight = []
+
+try:
+    with open(fileName) as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            strLength.append(row[0])
+            strHeight.append(row[1])
+        print("The data has been accessed!")
+except Exception:
+    print(fileName+" is not found!\n"+"Check the name of the file!")
+
+# Change the Strings into Floats
+floatLength = [float(i) for i in strLength]
+floatHeight = [float(i) for i in strHeight]
+
+
+plt.figure(figsize=(20, 10), dpi=100)
+plt.title("Height change",  fontsize=30)
+plt.xlabel("Length/Micrometer", fontsize=20)
+plt.ylabel("Height/Angstrom", fontsize=20)
+
+
+plt.plot(floatLength,floatHeight)
+
+savePath = "./" + fileName[:-4] +".jpg"
+
+plt.show()
+plt.savefig(savePath)
+
+print("The figure has been saved!")
